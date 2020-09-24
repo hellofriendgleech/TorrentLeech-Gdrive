a6654b875c72656945c54b380b57935ed014f130
hellofriendgleech <71806482+hellofriendgleech@users.noreply.github.com>
2020-09-24 11:05:41 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,11 +2,12 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
-    DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+    TG_BOT_TOKEN= "1177066431:AAHyN0PxJ9B_Vw5GPwbRspFHcy9u3rxUM8M"
+    APP_ID = 1474808
+    API_HASH = "10c9e59fb3bfaee28bb6e76f7d5a6855"
+    OWNER_ID = 1283735480
+    AUTH_CHANNEL = [-477648128]
+    DESTINATION_FOLDER = "gleech" #Name of your folder read readme
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMDct2uWnbN_LJDR-wTSUu3N5AKTDHehBWNjGGpRsHkKihW4F1PGaeYmRHsYf1-B8DBdHjhw5ybX0_oMs7ge1q_bCT0NDtkf7Pln_vr8flKzW11dU531wh_5N_EsKJh6I5sx3yk_taMXcaP9N8rVXrVeUtaTxxQ","token_type":"Bearer","refresh_token":"1//0gPZ3HWJ6W7YcCgYIARAAGBASNwF-L9IrNL8CeXTDU221kgJ5aspuIDlk4hM7WHcXf1LotnnIhEq7Bieuzw4zH2kUUxetBquIhFM","expiry":"2020-06-23T18:32:03.76910669Z"}
+root_folder_id = 18iWw_jEh2G4CNXMKuxwUm9h6HPfPCxzp}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
